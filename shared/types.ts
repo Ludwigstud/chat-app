0c88c737b36d4e8bc702c8f55662dcc9d0550c57
ludwig <ludwigstridh@gmail.com>
2025-11-19 10:55:11 +0100

    defined types and added shared folder

---
+++ shared/types.ts
@@ -0,0 +1,39 @@
+export interface IMessage {
+	_id?: string;
+
+	userId: string;
+
+	message: string;
+
+	date: Date;
+}
+
+export interface IChatroom {
+	_id?: string;
+
+	name: string;
+
+	users: string[];
+
+	chatLogs: IMessage[];
+
+	createdAt?: Date;
+
+	updatedAt?: Date;
+}
+
+export interface IUser {
+	_id?: string;
+
+	username: string;
+
+	password: string;
+
+	email: string;
+
+	chatrooms: string[];
+
+	createdAt?: Date;
+
+	updatedAt?: Date;
+}
